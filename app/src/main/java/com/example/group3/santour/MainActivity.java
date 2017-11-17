9dd663f8b9b7767531b5af59b75ebb75c79e1e01
PC-PEDRO\pedro <pedro.f_1870@hotmail.com>
2017-11-17 09:45:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/group3/santour/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.group3.santour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
