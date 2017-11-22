c6b3b372728e3ebf7aca62d5b9c3a0e6d8a4b077
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2017-11-22 18:41:09 +0100

    added small map on first activity

--- app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
+++ app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
@@ -1,18 +1,79 @@
 package com.example.group3.santour.Activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.group3.santour.Firebase.ResetDB;
+import com.example.group3.santour.Logic.Permissions;
+import com.example.group3.santour.Logic.Record;
 import com.example.group3.santour.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ResetDB.resetDB();
+
+
+        mapView = (MapView) findViewById(R.id.mapView3);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        //first ask for the permissions
+        Permissions.checkPermissions(MainActivity.this);
+
+        //Create a new Record object
+        Record record = new Record();
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    
 }
+
+
---
+++ app/src/main/java/com/example/group3/santour/Logic/Record.java
@@ -0,0 +1,22 @@
+package com.example.group3.santour.Logic;
+
+/**
+ * Created by aleks on 22.11.2017.
+ */
+
+public class Record {
+
+    public Record(){
+
+    }
+
+    public void startRecording(){
+
+    }
+
+    public void getUserPosition(){
+
+    }
+
+
+}
