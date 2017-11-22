f5ecb9ef51a05f9b49848f2658a2549c79c10e9d
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2017-11-22 14:37:22 +0100

    first commit branch

--- app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
+++ app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.group3.santour.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.group3.santour.Firebase.ResetDB;
 import com.example.group3.santour.R;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Test", "frontend branch");
         ResetDB.resetDB();
 
     }
