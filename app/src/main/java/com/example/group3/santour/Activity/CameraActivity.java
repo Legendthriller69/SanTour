b3f31037a3114a18163e52cda2ddbea72642db36
0xk3v <kevinschmidt_113@hotmail.com>
2017-11-24 11:35:46 +0100

    Cam and b64 db storage

--- app/src/main/java/com/example/group3/santour/Activity/CameraActivity.java
+++ app/src/main/java/com/example/group3/santour/Activity/CameraActivity.java
@@ -2,14 +2,21 @@ package com.example.group3.santour.Activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.group3.santour.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by kevin on 23/11/17.
@@ -18,6 +25,7 @@ import com.example.group3.santour.R;
 public class CameraActivity extends AppCompatActivity {
 
     ImageView mImageView ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,7 @@ public class CameraActivity extends AppCompatActivity {
             //On click function
             public void onClick(View view) {
                 //Create the intent to start another activity
-                takePictureIntent();
+                launchCamera();
             }
         });
 
@@ -61,12 +69,14 @@ public class CameraActivity extends AppCompatActivity {
         });
     }
 
+
+    //If Request_image_capture > 0 return -> OnActivityResult
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     /**
      * Method to open camera intent and take a picture - returns back to application
      */
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void takePictureIntent() {
+    private void launchCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -84,9 +94,38 @@ public class CameraActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+            //mImageView.setImageBitmap(imageBitmap);
+            encodeBitmapAndSaveToFirebase(imageBitmap);
         }
     }
 
+    /**
+     * Save image into the DB -> Base64 encoded -> NEED TO MODIFY HOW TO RETRIEVE CHILDS BECAUSE HARDCODED
+     * @param bitmap
+     */
+    public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("tracks")
+                .child("-KzVcArNHH3TgHscp6A2")
+                .child("pois")
+                .child("0")
+                .child("picture");
+        ref.setValue(imageEncoded);
+        Log.i("YOLO-OL", "encodeBitmapAndSaveToFirebase: " + imageEncoded);
+        decodeB64Bitmap(imageEncoded);
+    }
+
+    /**
+     * Decode the b64 String directly when encoded to test
+     * @param encodedImage
+     */
+    public void decodeB64Bitmap(String encodedImage){
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        mImageView.setImageBitmap(decodedByte);
+    }
 
 }
