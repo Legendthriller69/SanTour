45c053efab26c955a45a50b764bab36c58d1c145
JuliePreperier <julie.preperier@netplus.ch>
2017-11-24 14:16:09 +0100

    Gui record track ok

--- app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
+++ app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.group3.santour.Activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,12 +12,21 @@ import com.example.group3.santour.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Test", "frontend branch");
-        ResetDB.resetDB();
+
+        fragmentManager = getSupportFragmentManager();
+
+        this.setTitle(this.getString(R.string.start_recording));
+
+        fragment = new Record_Fragment();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_container, fragment).commit();
 
     }
 }
