8ec78b201c5e376b35471efe0d35f76abfc8e7d4
JuliePreperier <julie.preperier@netplus.ch>
2017-11-27 15:37:46 +0100

    BottomNavigation start

--- app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
+++ app/src/main/java/com/example/group3/santour/Activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.group3.santour.Activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.group3.santour.Logic.Permissions;
 import com.example.group3.santour.R;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Fragment fragment;
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
+    private BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +30,42 @@ public class MainActivity extends AppCompatActivity {
         Permissions permissions = new Permissions();
         permissions.checkPermissions(this);
 
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.inflateMenu(R.menu.navigation);
+
         fragmentManager = getSupportFragmentManager();
+
+        this.setTitle(this.getString(R.string.start_recording));
+
+
 
         fragment = new Record_Fragment();
         transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.main_container, fragment).commit();
 
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.navigation_about:
+                        fragment = new Pod_Fragment(); // A CHANGER
+                        break;
+                    case R.id.navigation_tracks:
+                        fragment = new Poi_Fragment(); // A CHANGER
+                        break;
+                    case R.id.navigation_settings:
+                        fragment = new Record_Fragment();
+                        break;
+                }
+
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                transaction.addToBackStack(null);
+
+                return true;
+            }
+        });
     }
 
     @Override
